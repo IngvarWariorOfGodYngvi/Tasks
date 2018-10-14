2ed4ce32f9ba2c374ee9e540c416fd05a0d9f174
Igor Ingvar Żebrowski <i.zebrowski.ul@gmail.com>
2018-10-14 10:38:41 +0200

    implements quest 1

--- src/main/java/sum/Sum.java
+++ src/main/java/sum/Sum.java
@@ -1,6 +1,26 @@
 package sum;
 
-public class Sum {
-    double a,b;
+import java.util.Scanner;
 
+public class Sum {
+    static double a=0,b=0;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Będzie teraz robiona suma");
+        double sum = sum();
+        System.out.println("Suma argumentów domyślnych " + sum);
+        System.out.println("Podaj pierwszą liczbę");
+        a = scanner.nextInt();
+        System.out.println("Podaj drugą liczbę");
+        b = scanner.nextInt();
+        double sum2 = sum();
+        System.out.println("Suma wynosi: " + sum2);
+
+
+    }
+
+    static double sum(){
+        return a+b;
+    }
 }
