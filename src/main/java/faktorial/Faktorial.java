4277df1036b413ead2bd7316e0c90d7997fb84f6
Igor Ingvar Żebrowski <i.zebrowski.ul@gmail.com>
2018-10-14 12:32:43 +0200

    quest4

---
+++ src/main/java/faktorial/Faktorial.java
@@ -0,0 +1,37 @@
+package faktorial;
+
+import java.util.Scanner;
+
+public class Faktorial {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj liczbę z czego chcesz policzyć silnie za pomocą for");
+        int a = scanner.nextInt();
+        int wynik = silnia(a);
+        System.out.println("Podaj liczbę z czego chcesz policzyć silnie za pomocą rekurencji");
+        int b = scanner.nextInt();
+        int wynik2 = silnia_rekurencyjna(b);
+        System.out.println("Silnia z for: " + wynik +"\n");
+        System.out.println("Silnia rekurencyjna: " + wynik2);
+
+
+    }
+
+
+    public static int silnia(int n) {
+        int wynik  = 1;
+        for (int i = 1; i <= n; i++) {
+            wynik *= i;
+        }
+        return wynik;
+    }
+
+    public static int silnia_rekurencyjna (int n) {
+        if (n==0)
+            return 1;
+        else
+            return (n*silnia_rekurencyjna(n-1));
+    }
+}
+
---
+++ src/main/java/gnirst/Gnirst.java
@@ -0,0 +1,24 @@
+package gnirst;
+
+import java.util.Scanner;
+
+public class Gnirst {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Podaj swój tekst");
+        String userString = scanner.nextLine();
+        String reverse = reverseString(userString.toLowerCase());
+
+        System.out.println("Podałeś :" + userString + " Więc odwrotnie to będzie: " + reverse);
+    }
+
+    public static String reverseString(String s){
+        String stringOut = "";
+        for (int i=s.length()-1;i>=0;i--){
+        stringOut = stringOut+s.charAt(i);
+        }
+        return stringOut;
+    }
+
+}
