3e9b6e3f4a551116741030aefd92b969d2295bbb
Igor Ingvar Żebrowski <i.zebrowski.ul@gmail.com>
2018-10-14 11:40:03 +0200

    quest2

--- src/main/java/sum/Sum.java
+++ src/main/java/sum/Sum.java
@@ -6,14 +6,17 @@ public class Sum {
     static double a=0,b=0;
 
     public static void main(String[] args) {
+        for (int i = 0;i<args.length;i++){
+            System.out.println(args[i]);
+        }
         Scanner scanner = new Scanner(System.in);
         System.out.println("Będzie teraz robiona suma");
         double sum = sum();
         System.out.println("Suma argumentów domyślnych " + sum);
         System.out.println("Podaj pierwszą liczbę");
-        a = scanner.nextInt();
+        a = scanner.nextDouble();
         System.out.println("Podaj drugą liczbę");
-        b = scanner.nextInt();
+        b = scanner.nextDouble();
         double sum2 = sum();
         System.out.println("Suma wynosi: " + sum2);
 
--- src/main/java/zad2/Zad2.java
+++ src/main/java/zad2/Zad2.java
@@ -1,4 +1,28 @@
 package zad2;
 
+import java.util.Scanner;
+
 public class Zad2 {
+    static double N;
+
+    public static void main(String[] args) {
+
+        System.out.println("Podaj jakąś liczbę");
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+
+        int sum = 0;
+        int sum2 = 0;
+        for (int i = 0; i<=N; i++){
+            if (i%3 ==0){sum+=i;}
+            if (i%5 ==0){sum2+=i;}
+        }
+
+        System.out.println(sum);
+        System.out.println(sum2);
+
+
+
+
+    }
 }
